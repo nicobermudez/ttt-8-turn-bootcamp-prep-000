5611f94c79dc66927bcd76830aa0d4763395e92d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-11 18:42:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,21 @@ def position_taken?(board, index)
     true
   end
 end 
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
